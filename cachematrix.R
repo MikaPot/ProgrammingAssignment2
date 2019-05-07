3cb1ce0fbd45d98e5d5be85c291a9c08db31dd94
MikaPot <mikapot627@gmail.com>
2019-05-07 14:04:16 +0900

    assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- matrix()
+	set <- function(y){
+		x <<- y
+		m <<- NA
+	}
+	get <- function() x
+	setin <- function(solved) m <<- solved
+	getin <- function() m
+	matrix(c(set, get, setin, getin), 2, 2)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x[[4]]()
+	if(!is.na(m[[1]])){
+		message("getting cached data")
+		return(m)	
+	}
+	data <- x[[2]]()
+	m <- solve(data)
+	x[[3]](m)
+	m
 }
